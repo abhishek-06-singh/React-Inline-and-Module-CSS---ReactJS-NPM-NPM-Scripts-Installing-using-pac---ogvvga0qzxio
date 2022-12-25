40586cb9c3b9a0d878719d862e4545412768efbc
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2022-12-25 14:44:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import React from "react";
+import "../styles/App.css";
+import styles from "../styles/appStyles.module.css";
+
 const App = () => {
-const heading ={
-
-
-}
+  const heading = {
+    color: "red",
+    fontSize: "50px",
+  };
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className="success">Success</h1>
+      <h1 id="error" style={heading}>
+        Error
+      </h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
